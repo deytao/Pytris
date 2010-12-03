1095b44a7974f050bb258fb7586986a479668dac
Jonathan Camile <jonathan.camile+github@gmail.com>
2010-12-03 15:22:07 +0100

    Model design: Added shapes file containing shapes' class

---
+++ shapes.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+
+class Shape(object):
+    column = 0
+    line = 0
+    position = 0
+
+    def __init__(self):
+        return self
+
+    def move(self, moves):
+        self.column = self.column + moves.horizontal
+        self.column = self.column + moves.vertical
