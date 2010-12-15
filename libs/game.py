fca7469800f396a43dbbc70b103d182525c638bc
Jonathan Camile <jonathan.camile+github@gmail.com>
2010-12-15 11:00:15 +0100

    Shapes Drawing: Key bindings and random shapes' picking
        - Added shapes' list using to pick randomly shape's objects
        - Update key bindings to fit with vim's ones

--- libs/game.py
+++ libs/game.py
@@ -1,5 +1,6 @@
 
 import curses
+import random
 
 from curses import wrapper as run_cursed
 from model import shapes    # application's module
@@ -15,7 +16,6 @@ class Controler(object):
     
     def __init__(self, shapes_window):
         self.shapes_window = shapes_window
-        self.current_shape = shapes.I()
 
     def right(self):
         #XXX move current shape to left
@@ -25,14 +25,26 @@ class Controler(object):
 
     def left(self):
         #XXX move current shape to left
-        print_lines(['To the left!'], self.shapes_window, 30, 10)
+        shape = self.current_shape
+        shape.move(-3, 0)
+        print_lines(shape.states[0], self.shapes_window, shape.line, shape.column)
 
     def down(self):
         #XXX move current shape to left
-        print_lines(['Faaaaaalling!'], self.shapes_window, 30, 10)
+        shape = self.current_shape
+        shape.move(0, 1)
+        print_lines(shape.states[0], self.shapes_window, shape.line, shape.column)
 
 
 def run(main_window):
+    shapetypes = (
+        shapes.I,
+        shapes.J,
+        shapes.L,
+        shapes.S,
+        shapes.T,
+        shapes.Z,
+    )
     curses.curs_set(0)
     game_window = main_window.subwin(50, 25, 0, 0)
     print_lines([
@@ -44,19 +56,27 @@ def run(main_window):
         '|_|_| |_|_| |_|   |_|_| |_|_|_|',
     ], game_window)
     shapes_window = main_window.subwin(50, 32, 0, 26)
-    shapes_window.border()
+    shapes_window.timeout(500)
+    shapes_window.border(0)
     shapes_window.refresh()
     controler = Controler(shapes_window)
+    controler.current_shape = random.choice(shapetypes)()
     action_map = {
         'q': exit,
-        'o': controler.right,
-        'p': controler.left,
-        'l': controler.down,
+        'l': controler.right,
+        'h': controler.left,
+        'j': controler.down,
+        'k': controler.rotate,
     }
     while True:
-        keystroke = shapes_window.getch()  # blocking call
+        keystroke = shapes_window.getch()  # blocking call, returning -1 after timeout delay
+        shapes_window.clear()
+        shapes_window.border(0)
         try:
-            action = action_map[chr(keystroke)]
+            if keystroke == -1:
+                action = action_map['j']
+            else:
+                action = action_map[chr(keystroke)]
         except KeyError:
             pass
         else:
