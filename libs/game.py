9651e14ef4642fe3de44acd3d417306a2a6391ad
Jonathan <jonathan.camile+github@gmail.com>
2010-12-23 18:19:31 +0100

    User Interface: Updated displaying methods
        - Handle game area to not cross borders
        - Handle multi shapes

--- libs/game.py
+++ libs/game.py
@@ -13,41 +13,11 @@ def print_lines(lines, window, minrow=1, mincol=1):
             window.addstr(minrow + i, mincol, line)
         except Exception:
             pass
-    window.refresh()
 
 
 class Controler(object):
     
-    def __init__(self, shapes_window):
-        self.shapes_window = shapes_window
-
-    def right(self):
-        #XXX move current shape to left
-        shape = self.current_shape
-        shape.move(3, 0)
-        print_lines(shape.states[0], self.shapes_window, shape.line, shape.column)
-
-    def left(self):
-        #XXX move current shape to left
-        shape = self.current_shape
-        shape.move(-3, 0)
-        print_lines(shape.states[0], self.shapes_window, shape.line, shape.column)
-
-    def down(self):
-        #XXX move current shape to left
-        shape = self.current_shape
-        shape.move(0, 1)
-        print_lines(shape.states[0], self.shapes_window, shape.line, shape.column)
-
-    def rotate(self):
-        #XXX move current shape to left
-        shape = self.current_shape
-        states = shape.states
-        states.rotate(1)
-
-def run(main_window):
-    timeout = 500
-    shapetypes = (
+    shape_types = (
         shapes.I,
         shapes.J,
         shapes.L,
@@ -55,6 +25,52 @@ def run(main_window):
         shapes.T,
         shapes.Z,
     )
+
+    def __init__(self, shapes_window):
+        self.shapes_window = shapes_window
+
+    def right(self):
+        #XXX move current shape to left
+        shape = self.current_shape
+        shape.move(3, 0)
+
+    def left(self):
+        #XXX move current shape to left
+        shape = self.current_shape
+        if (shape.column > 0):
+            shape.move(-3, 0)
+
+    def down(self):
+        #XXX move current shape to left
+        shape = self.current_shape
+        shape.move(0, 1)
+
+    def rotate(self):
+        #XXX move current shape to left
+        shape = self.current_shape
+        states = shape.states
+        states.rotate(1)
+
+    def display(self, previous_ones):
+        #XXX print current shape
+        shape = self.current_shape
+        previous_ones.append(shape)
+        for i, displayed_shape in enumerate(previous_ones):
+            print_lines(displayed_shape.states[0], self.shapes_window, displayed_shape.line, displayed_shape.column)
+
+    def is_endoflines(self):
+        window = self.shapes_window
+        window_height, window_width = window.getmaxyx()
+        shape = self.current_shape
+        return (shape.line + len(shape.states[0]) >= window_height - 1)
+
+    def renew_shape(self):
+        self.current_shape = random.choice(self.shape_types)()
+
+
+def run(main_window):
+    timeout = 500
+    previous_shapes = []
     curses.curs_set(0)
     game_window = main_window.subwin(50, 25, 0, 0)
     print_lines([
@@ -64,7 +80,7 @@ def run(main_window):
     shapes_window.border(0)
     shapes_window.refresh()
     controler = Controler(shapes_window)
-    controler.current_shape = random.choice(shapetypes)()
+    controler.renew_shape()
     action_map = {
         'q': exit,
         'l': controler.right,
@@ -90,7 +106,12 @@ def run(main_window):
             pass
         else:
             action()
+        if (controler.is_endoflines()):
+            previous_shapes.append(controler.current_shape)
+            controler.renew_shape()
+        controler.display(previous_shapes)
         shapes_window.timeout(int(timeout))
+        shapes_window.refresh()
 
 
 def main():
