83d94037dc0b26a1a44ae465c555b96ce92f5c57
Jonathan Camile <jonathan.camile+github@gmail.com>
2015-08-09 19:33:17 +0200

    First step through the matrix

--- libs/game.py
+++ libs/game.py
@@ -1,5 +1,6 @@
 import curses
 
+from copy import copy
 from curses import wrapper as run_cursed
 from model import shapes    # application's module
 
@@ -10,11 +11,51 @@ SPEED = 500
 def print_lines(lines, window, minrow=1, mincol=1):
     for i, line in enumerate(lines):
         window.addstr(minrow + i, mincol, line)
-    window.refresh()
+
+
+class Playfield(object):
+    """
+        Shapes container
+    """
+
+    def __init__(self, window, width, height, x=0, y=0):
+        self.window = window.subwin(height * 3, width * 3, y, x)
+        self.base_matrix = {(x, y): 0 for x in range(width) for y in range(height)}
+        self.shape = None
+        self.matrix = None
+
+    def set_shape(self, shape):
+        self.shape = shape
+
+    def _merge(self):
+        self.matrix = copy(self.base_matrix)
+        if not self.shape:
+            return
+        for point, state in self.shape.current_state.items():
+            x, y = point
+            new_point = (x + self.shape.xcoord, y + self.shape.ycoord)
+            self.matrix[new_point] = state
+
+    def print(self):
+        self._merge()
+        self.window.clear()
+        self.window.border()
+        for point, state in self.matrix.items():
+            if not state:
+                continue
+            x, y = (p + (p - 1) for p in point)
+            self.window.addstr(y - 1, x, '_')
+            self.window.addstr(y, x - 1, '|')
+            self.window.addstr(y, x, '_')
+            self.window.addstr(y, x + 1, '|')
+        self.window.refresh()
 
 
 class Controler(object):
-    
+    """
+        Gamepad
+    """
+
     def __init__(self, window):
         self.window = window
         self.current_shape = shapes.Shape.get()
@@ -23,30 +64,30 @@ class Controler(object):
         """ rotate current shape clockwise """
         shape = self.current_shape
         shape.state = shape.state + 1
-        print_lines(shape.current_state, self.window, shape.line, shape.column)
+        return shape
 
     def left(self):
         """ move current shape to the left """
         shape = self.current_shape
         shape.move(-1, 1)
-        print_lines(shape.current_state, self.window, shape.line, shape.column)
+        return shape
 
     def down(self):
         """ move current shape to the bottom """
         shape = self.current_shape
         shape.move(0, 1)
-        print_lines(shape.current_state, self.window, shape.line, shape.column)
+        return shape
 
     def right(self):
         """ move current shape to the right """
         shape = self.current_shape
         shape.move(1, 1)
-        print_lines(shape.current_state, self.window, shape.line, shape.column)
+        return shape
 
 
 def run(main_window):
     curses.curs_set(0)
-    game_window = main_window.subwin(50, 25, 0, 0)
+    side = Playfield(main_window, 17, 8)
     print_lines([
         'Welcome to Pytris',
         '             _',
@@ -54,11 +95,11 @@ def run(main_window):
         '|_|    _ _  |_|  ___       _',
         '|_|_  |_|_| |_| |_|_|_   _|_|_',
         '|_|_| |_|_| |_|   |_|_| |_|_|_|',
-    ], game_window)
-    shapes_window = main_window.subwin(50, 40, 0, 26)
-    shapes_window.border()
-    shapes_window.refresh()
-    controler = Controler(shapes_window)
+        ], side.window)
+    side.window.refresh()
+    playfield = Playfield(main_window, 17, 13, x=25)
+    playfield.print()
+    controler = Controler(playfield.window)
     action_map = {
         'q': exit,
         'w': controler.rotate,
@@ -66,9 +107,9 @@ def run(main_window):
         's': controler.down,
         'd': controler.right,
     }
-    shapes_window.timeout(SPEED)
+    playfield.window.timeout(SPEED)
     while True:
-        keystroke = shapes_window.getch()
+        keystroke = playfield.window.getch()
         if keystroke == curses.ERR:
             keystroke = ord('s')
         try:
@@ -76,9 +117,9 @@ def run(main_window):
         except (KeyError, ValueError):
             continue
         try:
-            shapes_window.clear()
-            shapes_window.border()
-            action()
+            shape = action()
+            playfield.set_shape(shape)
+            playfield.print()
         except curses.error:
             pass
 
